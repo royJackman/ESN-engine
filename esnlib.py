e053c62ca37ec3edd7a1ddefd02e7539e55c1203
Roy Jackman <jackman.roy@gmail.com>
2018-08-05 08:46:48 -0400

    Graphgen fix
    Fixed the graph generator to include enough 3-cliques

--- esnlib.py
+++ esnlib.py
@@ -97,26 +97,43 @@ def graphGen(n=128, p=0.3, seed=1618, circ=False, verbose=False):
                         retval[x,y] = np.random.randint(-15,16)
     
     else:
-        # Make a ring oscillator first
+        # Make a degree 2 ring oscillator first
         for x in range(n):
             retval[x,(x+1)%n] = random.choice(weights)
+            retval[(x+2)%n,x] = random.choice(weights)
 
+        # Build the fractal pattern within the network by filling a loops array
+        # then adding the edges, emptying it, and repeating with a loop half
+        # the size of the last 
         o = n
         loops = []
+
+        # At size 4 and below, nothing else is needed because it was taken care
+        # of during the degree 2 ring oscillator
         while o > 4:
             if verbose: print(o)
+            
+            # If the loop is odd...
             if o % 2 == 1:
+                # Get the 'half'
                 p = (o-1)/2
+                # If the loops array is empty...
                 if len(loops) == 0:
+                    # Must be the first one, so add two loops and that's it
                     loops.append((0,p+1))
                     loops.append((p,0))
+                # If the loops array is NOT empty...
                 else:
+                    # Copy the existing loops and reset the loops array
                     temp = loops
                     loops = []
+                    # Iterate through the existing loops and cut them in two
                     for t in temp:
                         loops.append(((t[1]+p),t[1]))
                         loops.append((t[0],(t[1]+p)))
+            # If the loop is even...
             else:
+                # Do things that are similar
                 p = (o-2)/2
                 if len(loops) == 0:
                     loops.append((p,0))
@@ -130,17 +147,6 @@ def graphGen(n=128, p=0.3, seed=1618, circ=False, verbose=False):
             o = p+1
             if verbose: print(loops)
             for l in loops:
-                retval[int(l[0]),int(l[1])] = random.choice(weights)
-        if o in [3,4]:
-            if len(loops) == 0:
-                loops.append((2,0))
-            else:
-                temp = loops
-                loops = []
-                for t in temp:
-                    loops.append(((t[1]+2),t[1]))
-        if verbose: print(loops)
-        for l in loops:
                 retval[int(l[0]),int(l[1])] = random.choice(weights)
 
     # Return the weight matrix
